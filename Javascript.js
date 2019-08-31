0275f3e373ef19588481f8dc5981d2cf877fb53e
C00P3RR <54748669+C00P3RR@users.noreply.github.com>
2019-08-31 18:36:57 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+function init() {
+Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1Na44BhYCXnVx9D9J3COsU-y5NE5Hmk47KQzEZ88RVaM/edit?usp=sharing',
+callback: function(data, tabletop) {
+console.log(data)
+},
+simpleSheet: true } )
+}
+window.addEventListener(‘DOMContentLoaded’, init)
